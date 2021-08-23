e3e34d306c60f305ed4427a0bff1ad020045f1a5
Batareika007 <batareika666@gmail.com>
2021-08-23 14:52:04 +0300

    added sound, now sound work on click

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
 let btns = document.querySelectorAll('button');
-// let j = new Audio("sounds/crash.mp3")
-
-let crashSound = new Audio("sounds/crash.mp3")
-let kickBassSound = new Audio("sounds/kick-bass")
-let snareSound = new Audio("sounds/snare.mp3")
-let tom1Sound = new Audio("sounds/tom-1.mp3")
-let tom2Sound = new Audio("sounds/tom-2.mp3")
-let tom3Sound = new Audio("sounds/tom-3.mp3")
-let tom4Sound = new Audio("sounds/tom-4.mp3")
 
 for (i of btns) {
-  i.addEventListener('click', function() {
-    console.log(this);
+  i.addEventListener('click', function () {
+    let buttonKey = this.innerHTML;
+    console.log(buttonKey);
     this.classList.toggle("whiteColor");
     // let soundLetter = this.textContent;
-    
-    
-    
-    
+
+    switch (buttonKey) {
+      case "w":
+        let tom1Sound = new Audio("sounds/tom-1.mp3");
+        tom1Sound.play();
+        break;
+
+      case "a":
+        let tom2Sound = new Audio("sounds/tom-2.mp3");
+        tom2Sound.play();
+        break;
+
+      case "s":
+        let tom3Sound = new Audio("sounds/tom-3.mp3");
+        tom3Sound.play();
+        break;
+
+      case "d":
+        let tom4Sound = new Audio("sounds/tom-4.mp3");
+        tom4Sound.play();
+        break;
+
+      case "j":
+        let snareSound = new Audio("sounds/snare.mp3");
+        snareSound.play();
+        break;
+
+      case "k":
+        let crashSound = new Audio("sounds/crash.mp3");
+        crashSound.play();
+        break;
+
+      case "l":
+        let kickBassSound = new Audio("sounds/kick-bass");
+        kickBassSound.play();
+        break;
+      default:
+    }
+
+
+
     // console.log(soundLetter);
   });
 }
-
-
-
-
-
-
 
 
 // tom1Sound.play();
